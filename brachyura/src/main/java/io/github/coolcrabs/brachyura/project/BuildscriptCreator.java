a044d59125a0544999f4bcd4200a14c06e06d113
Geolykt <admin@geolykt.de>
2022-05-07 17:00:46 +0200

    Improved project creator API

--- brachyura/src/main/java/io/github/coolcrabs/brachyura/project/BuildscriptCreator.java
+++ brachyura/src/main/java/io/github/coolcrabs/brachyura/project/BuildscriptCreator.java
@@ -12,11 +12,17 @@ import org.jetbrains.annotations.NotNull;
 
 import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.CodeBlock;
+import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 
+import io.github.coolcrabs.brachyura.dependency.JavaJarDependency;
+import io.github.coolcrabs.brachyura.maven.HttpMavenRepository;
 import io.github.coolcrabs.brachyura.maven.MavenId;
+import io.github.coolcrabs.brachyura.maven.MavenResolver;
 import io.github.coolcrabs.brachyura.project.java.SimpleJavaProject;
 
 // Slbrachyura: Interactive project creation
@@ -25,6 +31,8 @@ public class BuildscriptCreator {
     private String buildscriptName = "Buildscript";
     private String buildscriptProjectName = "ExampleBuildscript";
     private Type buildscriptSuperclass = SimpleJavaProject.class;
+    private List<MavenId> dependencies = new ArrayList<>();
+    private List<String> foreignRepositories = new ArrayList<>();
     private int javaVersion = 8;
     private MavenId projectId = new MavenId("com.example", "example", "0.0.1-SNAPSHOT");
 
@@ -44,6 +52,7 @@ public class BuildscriptCreator {
         }
 
         List<MethodSpec> methods = new ArrayList<>();
+        List<FieldSpec> fields = new ArrayList<>();
 
         methods.add(MethodSpec.methodBuilder("getId")
                 .returns(MavenId.class)
@@ -69,9 +78,39 @@ public class BuildscriptCreator {
                 .addStatement(CodeBlock.of("return $S", buildscriptProjectName))
                 .build());
 
+        if (!dependencies.isEmpty() || !foreignRepositories.isEmpty()) {
+            TypeName jarDepArrayList = ParameterizedTypeName.get(ArrayList.class, JavaJarDependency.class);
+            FieldSpec field = FieldSpec.builder(jarDepArrayList, "dependencies", Modifier.PRIVATE, Modifier.FINAL)
+                    .initializer("new $T()", jarDepArrayList)
+                    .build();
+            fields.add(field);
+            MethodSpec.Builder builder = MethodSpec.methodBuilder("createDependencies")
+                    .addModifiers(Modifier.PUBLIC)
+                    .addAnnotation(Override.class)
+                    .addAnnotation(NotNull.class)
+                    .returns(jarDepArrayList)
+                    .addStatement("$T list = new $T(super.createDependencies())", jarDepArrayList, jarDepArrayList)
+                    .beginControlFlow("if ($N.isEmpty())", field)
+                    .addStatement("$T resolver = new $T($T.MAVEN_LOCAL)", MavenResolver.class, MavenResolver.class, MavenResolver.class);
+
+            for (String repo : foreignRepositories) {
+                builder.addStatement("resolver.addRepository(new $T($S))", HttpMavenRepository.class, repo);
+            }
+
+            for (MavenId dep : dependencies) {
+                builder.addStatement("$N.add(resolver.getJarDepend(new $T($S, $S, $S)))", field, MavenId.class, dep.groupId, dep.artifactId, dep.version);
+            }
+
+            methods.add(builder.endControlFlow()
+                    .addStatement("list.addAll($N)", field)
+                    .addStatement("return list")
+                    .build());
+        }
+
         TypeSpec type = TypeSpec.classBuilder(className)
                 .addSuperinterface(DescriptiveBuildscriptName.class)
                 .superclass(buildscriptSuperclass)
+                .addFields(fields)
                 .addMethods(methods)
                 .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
                 .build();
@@ -93,6 +132,20 @@ public class BuildscriptCreator {
     @Contract(mutates = "this", pure = false, value = "_ -> this")
     public BuildscriptCreator withClassName(@NotNull String name) {
         this.buildscriptName = name;
+        return this;
+    }
+
+    @NotNull
+    @Contract(mutates = "this", pure = false, value = "_ -> this")
+    public BuildscriptCreator withDependency(@NotNull MavenId dep) {
+        this.dependencies.add(dep);
+        return this;
+    }
+
+    @NotNull
+    @Contract(mutates = "this", pure = false, value = "_ -> this")
+    public BuildscriptCreator withHttpRepo(@NotNull String repoURL) {
+        this.foreignRepositories.add(repoURL);
         return this;
     }
 
